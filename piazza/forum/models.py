ad592244c1e816473a20b96f59756701779add78
ayushkumar2000 <46759425+ayushkumar2000@users.noreply.github.com>
2019-03-21 13:26:41 +0530

    Add files via upload

--- piazza/forum/models.py
+++ piazza/forum/models.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import reverse
 from datetime import datetime, timezone
 # Create your models here.
 class thread(models.Model):
-    value=models.CharField(max_length=5000)
+    value=models.TextField(max_length=5000)
     subject=models.CharField(max_length=5000)
     author=models.CharField(max_length=100)
     created = models.DateTimeField()
@@ -28,7 +28,7 @@ class thread(models.Model):
 class comment(models.Model):
     Thread=models.ForeignKey(thread,on_delete=models.CASCADE)
     filetype=models.CharField(max_length=100)
-    comment=models.CharField(max_length=1000)
+    comment=models.TextField(max_length=1000)
     
     author=models.CharField(max_length=100)
     author_type = models.IntegerField()
